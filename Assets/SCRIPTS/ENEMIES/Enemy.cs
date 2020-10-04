4c0515cbb0ebb239e218522920b1248025a51915
Val <valeens12@gmail.com>
2020-10-04 17:41:24 +0200

    Audio + Enemies
    Coins audio and enemy fliping when player jumps on their heads

---
+++ Assets/SCRIPTS/ENEMIES/Enemy.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    void Update()
+    {
+        if(transform.position.y < -7)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
