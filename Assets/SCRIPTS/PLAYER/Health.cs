6d28044e7cf5d0ae61eb117c8a6ab387aec537cc
Val <valeens12@gmail.com>
2020-10-05 21:20:34 +0200

    Health system
    Trying to make empty and full hearts change by code.

---
+++ Assets/SCRIPTS/PLAYER/Health.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    public int health;
+    public int heartsNumb;
+
+    public Image[] hearts;
+    public Sprite fullHeart;
+    public Sprite emptyHeart;
+
+    void Update()
+    {
+        if(health > heartsNumb)
+        {
+            health = heartsNumb;
+        }
+
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if( i < health)
+            {
+                hearts[i].sprite = fullHeart;
+            }
+
+            else
+            {
+                hearts[i].sprite = emptyHeart;
+            }
+
+            if (i < heartsNumb)
+            {
+                hearts[i].enabled = true;
+            }
+
+            else
+            {
+                hearts[i].enabled = false;
+            }
+        }
+    }
+}
