eec25923b907c45661eafd1c137ccb85d4ff79ea
Val <valeens12@gmail.com>
2020-10-31 21:43:05 -0300

    High score.
    High score works but there is no text on the screen that says "High score".

--- Assets/SCRIPTS/SCORE/Score.cs
+++ Assets/SCRIPTS/SCORE/Score.cs
@@ -6,15 +6,23 @@ using UnityEngine.UI;
 public class Score : MonoBehaviour
 {
     public Text score;
+    public Text highScore;
     public static int scoreAmount;
 
     void Start()
     {
         score = GetComponent<Text>();
         scoreAmount = 0;
+        highScore.text = PlayerPrefs.GetInt("HighScore", 0).ToString();
     }
     void Update()
     {
         score.text = "SCORE: " + scoreAmount;
+
+        if(scoreAmount > PlayerPrefs.GetInt("HighScore", 0))
+        {
+            PlayerPrefs.SetInt("HighScore", scoreAmount);
+            highScore.text = scoreAmount.ToString();
+        }
     }    
 }
